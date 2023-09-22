4f27d91ec82743b2edd97096c6e1058854137235
alhussien almasmary <alhussien.almasmari11@gmail.com>
2023-09-22 11:53:37 +0300

    new updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,13 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swipper-partener1', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
-  
+    slidesPerView: 6.5,
+    spaceBetween: 132,
+    speed: 400,
+    autoplay: {
+        delay: 500,
+      },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
@@ -19,4 +24,31 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+  const swiper2 = new Swiper('.swipper-partener2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 6.5,
+    spaceBetween: 132,
+    speed: 400,
+    autoplay: {
+        delay: 500,
+      },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
   
